d155ca61a21ee3bc5d70f10bdde7f6e32f880af2
nikitasanin2006 <91720253+nikitasanin2006@users.noreply.github.com>
2025-01-09 20:43:44 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+import logging
+from create_bot import bot, dp, scheduler
+from handlers.start import start_router
+# from work_time.time_func import send_time_msg
+
+async def main():
+    # scheduler.add_job(send_time_msg, 'interval', seconds=10)
+    # scheduler.start()
+    dp.include_router(start_router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
